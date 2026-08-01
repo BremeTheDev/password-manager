9ca8a8f4b73a349671860fcf725c5abddc687327
BremeTheDev <athulthegoofball@gmail.com>
2026-08-01 21:08:40 +0530

    Added somewhat working encryption

--- add_pass.py
+++ add_pass.py
@@ -1,15 +1,25 @@
 import json
+from cryptography.fernet import Fernet
 def add():
     website = input("\nWebsite:")
     username = input("Username:")
     password = input("Password:")
+
+    key = Fernet.generate_key()
+    with open("key.key","wb") as file:
+        file.write(key)
+    with open("key.key","rb") as file:
+        file.read()
+    fernet = Fernet(key)
+    encrypted_password = fernet.encrypt(password.encode()).decode()
+
     with open("passwords.json","r") as file:
         data = json.load(file)
     if website in data:
         print("| PASSWORD ALREADY EXISTS! |")
     else:
         data[website] = {
-            username : password
+            username : encrypted_password
         }
         with open("passwords.json","w") as file:
             json.dump(data, file, indent=4)
--- view_pass.py
+++ view_pass.py
@@ -1,12 +1,17 @@
 import json
+from cryptography.fernet import Fernet
 def view():
     with open("passwords.json","r") as file:
         data_read = json.load(file)
         print("\n-----------------")
         print("| ALL PASSWORDS |")
         print("-----------------")
+        with open("key.key","rb") as file:
+            key = file.read()
+        fernet = Fernet(key)
         for website, data in data_read.items():
             for username, password in data.items():
+                decrypted_password = fernet.decrypt(password.encode()).decode()
                 print("\nWebsite:", website)
                 print("Username:", username)
-                print("Password:", password)
+                print("Password:", decrypted_password)
